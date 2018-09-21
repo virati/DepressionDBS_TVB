e2789f8015ff8953d10bf436569c6d934fcab400
Vineet Tiruvadi <virati@gmail.com>
2018-09-21 13:44:49 +0200

    Added license file and started merging GPL info into headers of scripts

--- kuramoto_net.py
+++ kuramoto_net.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+ Kuramoto network implemented in The Virtual Brain (http://thevirtualbrain.org/)
+
+    Copyright (C) 2018  Vineet Tiruvadi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 Created on Wed Sep 19 13:51:37 2018
 
 @author: virati
